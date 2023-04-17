39f4d63ac70e0906707f22ea5325591998190032
Juno Kim <lippoint.surf0622@gmail.com>
2023-04-17 19:15:19 +0900

    라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RouterComponent from './router';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterComponent />
+    </div>
+  );
 }
 
 export default App;
