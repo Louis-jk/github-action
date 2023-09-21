e9fbb811cb90e7f57ccab85845414500cbfb776b
Juno Kim <lippoint.surf0622@gmail.com>
2023-09-21 15:04:51 +0900

    change response send text and port

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/names', (req, res) => {
-  res.send('Telmo');
+  res.send('Joonho');
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(3500, () => {
+  console.log('Server is running on port 3500');
 });
